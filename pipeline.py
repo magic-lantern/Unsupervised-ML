3dca09b85646bf4f073ca5424125b4a6d9472211
vector-client-token <>
2020-12-02 16:03:56 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,9 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.e11e3d67-1166-4437-b00a-6d50cfee9586"),
+    inpatient_ml_dataset=Input(rid="ri.foundry.main.dataset.07927bca-b175-4775-9c55-a371af481cc1")
+)
+def unnamed(inpatient_ml_dataset):
+    
+
