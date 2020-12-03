3d7d86b9b91b16bd3ef4711a53cc96ae4f9e0622
vector-client-token <>
2020-12-03 18:53:29 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -173,7 +173,10 @@ def pca_ranked_features(inpatient_encoded):
     pc3_df = pc3_df.reset_index()
     pc3_df = pc3_df.rename(columns = {'index':'pc-3-col', 'PC-3': 'pc-3-val'})
 
-    return spark.createDataFrame(pd.concat([pc1_df, pc2_df, pc3_df], axis=1)).sort(col('pc-1-val').asc())
+    sdf = spark.createDataFrame(pd.concat([pc1_df, pc2_df, pc3_df], axis=1))
+    sdf = sdf.sort(col('pc-1-val').desc())
+
+    return sdf
 
 @transform_pandas(
     Output(rid="ri.vector.main.execute.2ac8a8fd-c395-4c49-84fe-3404d8ede65d"),
