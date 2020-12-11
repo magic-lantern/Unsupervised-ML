a4562d509af8a3d1026fdb1b5ce2b5fa627a88f0
vector-client-token <>
2020-12-11 17:00:23 +0000

    Update workbook

--- pipeline.sql
+++ pipeline.sql
@@ -13,7 +13,7 @@ order by s_year, s_month
     Output(rid="ri.foundry.main.dataset.87ae0fb9-7045-4180-a60a-2869682b6f8a"),
     inpatient_ml_dataset=Input(rid="ri.foundry.main.dataset.07927bca-b175-4775-9c55-a371af481cc1")
 )
-select count(1), year(visit_end_date) as s_year, month(visit_end_date) as s_month
+select count(1) as num_rec, year(visit_end_date) as s_year, month(visit_end_date) as s_month
 from inpatient_ml_dataset
 group by s_year, s_month
 order by s_year, s_month
