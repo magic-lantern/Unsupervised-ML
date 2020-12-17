527e5fc8330034799b418ea315a70110ab26768c
vector-client-token <>
2020-12-17 17:39:15 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -20,31 +20,8 @@ import plotly.express as px
 #np.set_printoptions(threshold=np.inf)
 
 @transform_pandas(
-    Output(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
-)
-def outcomes(inpatient_ml_dataset):
-    df = inpatient_ml_dataset
-    df = df.select('visit_occurrence_id',
-                   'person_id',
-                   'data_partner_id',
-                   'visit_concept_name',
-                   'covid_status_name',
-                   'in_death_table',
-                   'severity_type',
-                   'length_of_stay',
-                   'visit_start_date',
-                   'visit_end_date',
-                   'smoking_status',
-                   'blood_type',
-                   'ecmo',
-                   'aki_in_hospital',
-                   'invasive_ventilation',
-                   'testcount',
-                   'bad_outcome')
-    return df.toPandas()
-
-@transform_pandas(
-    Output(rid="ri.foundry.main.dataset.e6967b18-d64f-4539-9a9f-7ae3a5eef700")
+    Output(rid="ri.foundry.main.dataset.e6967b18-d64f-4539-9a9f-7ae3a5eef700"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def pca3_ranked_features( inpatient_scaled_w_imputation):
     dfo = inpatient_scaled_w_imputation
@@ -78,7 +55,7 @@ def pca3_ranked_features( inpatient_scaled_w_imputation):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.e60a9fc6-c946-4707-a1a0-bea11453ad48"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def pca_2_comp_analysis( inpatient_scaled_w_imputation, outcomes):
     # decent PCA guide available here: https://towardsdatascience.com/principal-component-analysis-pca-with-scikit-learn-1e84a0c731b0
@@ -118,7 +95,7 @@ def pca_2_comp_analysis( inpatient_scaled_w_imputation, outcomes):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.a230c6e9-ece6-46e0-89aa-c9414533899f"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c")
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def pca_3_comp_analysis( inpatient_scaled_w_imputation, outcomes):
     # decent PCA guide available here: https://towardsdatascience.com/principal-component-analysis-pca-with-scikit-learn-1e84a0c731b0
@@ -162,7 +139,8 @@ def pca_3_comp_analysis( inpatient_scaled_w_imputation, outcomes):
     return spark.createDataFrame(pd.DataFrame(pca_3.components_, columns=df.columns,index = ['PC-1','PC-2', 'PC-3']).reset_index())
 
 @transform_pandas(
-    Output(rid="ri.foundry.main.dataset.78eb8376-28de-4705-b6b1-d5d2cf520b45")
+    Output(rid="ri.foundry.main.dataset.78eb8376-28de-4705-b6b1-d5d2cf520b45"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def pca_all_dataset(inpatient_scaled_w_imputation):
     # decent PCA guide available here: https://towardsdatascience.com/principal-component-analysis-pca-with-scikit-learn-1e84a0c731b0
@@ -180,7 +158,8 @@ def pca_all_dataset(inpatient_scaled_w_imputation):
     return pd.DataFrame(pca_all_arr)
 
 @transform_pandas(
-    Output(rid="ri.foundry.main.dataset.ad26ec53-2c46-4d3c-9a78-c86c77accad7")
+    Output(rid="ri.foundry.main.dataset.ad26ec53-2c46-4d3c-9a78-c86c77accad7"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def pca_explained_variance( inpatient_scaled_w_imputation):
     df = inpatient_scaled_w_imputation
@@ -230,7 +209,6 @@ def pca_umap2d_embedding_stdscaled(pca_all_dataset):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.f7acb861-abfd-47a4-951a-4d0735ad9cf8"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     pca_umap2d_embedding_stdscaled=Input(rid="ri.foundry.main.dataset.d0d29902-d951-4b39-aacd-117a2c981f1e")
 )
 def pca_umap2d_scaled_viz_bad_outcome( outcomes, pca_umap2d_embedding_stdscaled):
@@ -250,7 +228,6 @@ def pca_umap2d_scaled_viz_bad_outcome( outcomes, pca_umap2d_embedding_stdscaled)
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.0636cf0d-202c-4919-bc65-06577254b95e"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     pca_umap2d_embedding_stdscaled=Input(rid="ri.foundry.main.dataset.d0d29902-d951-4b39-aacd-117a2c981f1e")
 )
 def pca_umap2d_scaled_viz_severity( outcomes, pca_umap2d_embedding_stdscaled):
@@ -271,7 +248,6 @@ def pca_umap2d_scaled_viz_severity( outcomes, pca_umap2d_embedding_stdscaled):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.6b6da43c-b691-45d8-a81a-b95def6bba59"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     pca_umap2d_embedding_stdscaled=Input(rid="ri.foundry.main.dataset.d0d29902-d951-4b39-aacd-117a2c981f1e")
 )
 def pca_umap2d_scaled_viz_site( outcomes, pca_umap2d_embedding_stdscaled):
@@ -294,7 +270,6 @@ def pca_umap2d_scaled_viz_site( outcomes, pca_umap2d_embedding_stdscaled):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.0b242849-8c9f-495c-955d-053da3a22e43"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     pca_umap2d_embedding=Input(rid="ri.foundry.main.dataset.d2ff4c8e-fbe2-445d-9df2-356a3a70e4f6")
 )
 def pca_umap2d_viz_bad_outcome( outcomes, pca_umap2d_embedding):
@@ -315,7 +290,6 @@ def pca_umap2d_viz_bad_outcome( outcomes, pca_umap2d_embedding):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.4f9b9075-269e-4aa6-bc77-d56491bce711"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     pca_umap2d_embedding=Input(rid="ri.foundry.main.dataset.d2ff4c8e-fbe2-445d-9df2-356a3a70e4f6")
 )
 def pca_umap2d_viz_severity( outcomes, pca_umap2d_embedding):
@@ -336,7 +310,6 @@ def pca_umap2d_viz_severity( outcomes, pca_umap2d_embedding):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.50e500ee-112f-41d6-8340-886b40d61b39"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     pca_umap2d_embedding=Input(rid="ri.foundry.main.dataset.d2ff4c8e-fbe2-445d-9df2-356a3a70e4f6")
 )
 def pca_umap2d_viz_site( outcomes, pca_umap2d_embedding):
@@ -360,7 +333,8 @@ def pca_umap2d_viz_site( outcomes, pca_umap2d_embedding):
 
 
 @transform_pandas(
-    Output(rid="ri.foundry.main.dataset.ba772263-cc7a-41ab-82a2-0203139bbbf4")
+    Output(rid="ri.foundry.main.dataset.ba772263-cc7a-41ab-82a2-0203139bbbf4"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def umap2d_embedding(inpatient_scaled_w_imputation):
     df = inpatient_scaled_w_imputation
@@ -375,7 +349,6 @@ def umap2d_embedding(inpatient_scaled_w_imputation):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.fd3540d6-d8ec-4374-a8d5-a573bf1292f5"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     umap2d_embedding=Input(rid="ri.foundry.main.dataset.ba772263-cc7a-41ab-82a2-0203139bbbf4")
 )
 def umap2d_viz_bad_outcome( outcomes, umap2d_embedding):
@@ -395,7 +368,6 @@ def umap2d_viz_bad_outcome( outcomes, umap2d_embedding):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.f9367601-0a80-489e-9958-4fd41a796713"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     umap2d_embedding=Input(rid="ri.foundry.main.dataset.ba772263-cc7a-41ab-82a2-0203139bbbf4")
 )
 def umap2d_viz_severity_type( outcomes, umap2d_embedding):
@@ -415,7 +387,6 @@ def umap2d_viz_severity_type( outcomes, umap2d_embedding):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.41b56b11-253f-4396-9ce9-71e381af695f"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     umap2d_embedding=Input(rid="ri.foundry.main.dataset.ba772263-cc7a-41ab-82a2-0203139bbbf4")
 )
 def umap2d_viz_site( outcomes, umap2d_embedding):
@@ -434,7 +405,8 @@ def umap2d_viz_site( outcomes, umap2d_embedding):
 
 
 @transform_pandas(
-    Output(rid="ri.foundry.main.dataset.c135a77f-4b71-4df9-abfe-be348abfc6a8")
+    Output(rid="ri.foundry.main.dataset.c135a77f-4b71-4df9-abfe-be348abfc6a8"),
+    inpatient_scaled_w_imputation=Input(rid="ri.foundry.main.dataset.bc823c17-fcdc-4801-a389-c6f476ed6971")
 )
 def umap3d_embedding(inpatient_scaled_w_imputation):
     df = inpatient_scaled_w_imputation
@@ -448,7 +420,6 @@ def umap3d_embedding(inpatient_scaled_w_imputation):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.58ae8bdf-979b-4847-ac9f-fdd4071c07ef"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     umap3d_embedding=Input(rid="ri.foundry.main.dataset.c135a77f-4b71-4df9-abfe-be348abfc6a8")
 )
 def umap3d_viz_bad_outcome(umap3d_embedding, outcomes):
@@ -468,7 +439,6 @@ def umap3d_viz_bad_outcome(umap3d_embedding, outcomes):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.4e12ebec-cf89-4018-813f-0ceefda14c1a"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     umap3d_embedding=Input(rid="ri.foundry.main.dataset.c135a77f-4b71-4df9-abfe-be348abfc6a8")
 )
 def umap3d_viz_severity_type(umap3d_embedding, outcomes):
@@ -488,7 +458,6 @@ def umap3d_viz_severity_type(umap3d_embedding, outcomes):
 
 @transform_pandas(
     Output(rid="ri.foundry.main.dataset.f1c093ec-5b1f-438a-894d-eb7acd69d8c9"),
-    outcomes=Input(rid="ri.foundry.main.dataset.3d9b1654-3923-484f-8db5-6b38b56e290c"),
     umap3d_embedding=Input(rid="ri.foundry.main.dataset.c135a77f-4b71-4df9-abfe-be348abfc6a8")
 )
 def umap3d_viz_site(umap3d_embedding, outcomes):
