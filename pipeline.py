8600d3cb030fc355bdb39bfffaad5fff5a34eed6
vector-client-token <>
2020-12-17 18:16:48 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -146,7 +146,7 @@ def pca_3_comp_analysis( inpatient_scaled_w_imputation, outcomes):
 )
 def pca_all_dataset(inpatient_scaled_w_imputation):
     # decent PCA guide available here: https://towardsdatascience.com/principal-component-analysis-pca-with-scikit-learn-1e84a0c731b0
-    df = inpatient_scaled_w_imputation
+    df = inpatient_scaled_w_imputation.toPandas()
     
     # take out visit_occurrence_id column
     df = df.drop(columns='visit_occurrence_id')
